a83973fde5aa3f8cb4ab830d1bd1aafe8d58291f
Brendan Porter <bporter@infinitydirect.com>
2025-02-23 10:36:36 -0600

    dumber yet

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() {
     // dbg!(check_safety("7 6 4 2 1".to_string(), -1));
 
     for line in contents.lines() {
-        if check_safety(line.to_string(), -1) {
+        if check_safety(line.to_string()) {
             safe_lines += 1
         }
         total_lines += 1
@@ -27,7 +27,7 @@ fn main() {
     // dbg!(check_safety("1 2 7 8 9".to_string()));
 }
 
-fn check_safety(input: String, skip_index: i32) -> bool {
+fn check_safety(input: String) -> bool {
     // Split input string into integers
     let binding = input.clone();
     let parts = binding.split(" ");
@@ -37,13 +37,53 @@ fn check_safety(input: String, skip_index: i32) -> bool {
     let mut decreasing = false;
     let mut gradual = true; // if increase/decrease is by 4 or more, gradual = false
     let mut last_number = -1;
-    let mut passing_before_last_index: bool = false;
+    let mut dampener_index = -1;
 
     for (index, part) in parts.clone().enumerate() {
         // convert part to int
         let part: i32 = part.parse().unwrap();
 
-        if skip_index == index as i32 {
+        if last_number == -1 {
+            last_number = part;
+            continue;
+        }
+        let diff = part - last_number;
+
+        if diff.abs() > 3 {
+            gradual = false;
+        }
+
+        if diff == 0 {
+            minimum_diff_met = false;
+        }
+        if diff < 0 {
+            decreasing = true;
+        } else {
+            increasing = true;
+        }
+
+        last_number = part;
+
+        if dampener_index == -1 && !((decreasing ^ increasing) & gradual & minimum_diff_met) {
+            dampener_index = index as i32
+        }
+    }
+
+    if (decreasing ^ increasing) & gradual & minimum_diff_met {
+        return true;
+    }
+
+    minimum_diff_met = true;
+    increasing = false;
+    decreasing = false;
+    gradual = true; // if increase/decrease is by 4 or more, gradual = false
+    last_number = -1;
+
+    for (index, part) in parts.clone().enumerate() {
+        // convert part to int
+        let part: i32 = part.parse().unwrap();
+
+        if index as i32 == dampener_index {
             continue;
         }
 
@@ -67,26 +107,6 @@ fn check_safety(input: String, skip_index: i32) -> bool {
         }
 
         last_number = part;
-
-        if skip_index == -1 && !((decreasing ^ increasing) & gradual & minimum_diff_met) {
-            if check_safety(input.clone(), index as i32) {
-                println!("removing index {} fixes report", index);
-                return true;
-            }
-        }
-
-        if (parts.clone().count() as i32) - 1 == index as i32 {
-            if skip_index == -1 {
-                let fixed = check_safety(input.clone(), 0);
-                if fixed {
-                    println!("removing index {} fixes report", index);
-                    return true;
-                }
-            }
-            return passing_before_last_index;
-        }
-
-        passing_before_last_index = (decreasing ^ increasing) & gradual & minimum_diff_met;
     }
 
     return (decreasing ^ increasing) & gradual & minimum_diff_met;
@@ -97,11 +117,18 @@ mod tests {
     use super::*;
     #[test]
     fn safe() {
-        assert_eq!(check_safety("7 6 4 2 1".to_string(), -1), true);
-        assert_eq!(check_safety("1 2 7 8 9".to_string(), -1), false);
-        assert_eq!(check_safety("9 7 6 2 1".to_string(), -1), false);
-        assert_eq!(check_safety("1 3 2 4 5".to_string(), -1), true);
-        assert_eq!(check_safety("8 6 4 4 1".to_string(), -1), true);
-        assert_eq!(check_safety("1 3 6 7 9".to_string(), -1), true);
+        assert_eq!(check_safety("7 6 4 2 1".to_string()), true);
+        assert_eq!(check_safety("1 2 7 8 9".to_string()), false);
+        assert_eq!(check_safety("9 7 6 2 1".to_string()), false);
+        assert_eq!(check_safety("1 3 2 4 5".to_string()), true);
+        assert_eq!(check_safety("8 6 4 4 1".to_string()), true);
+        assert_eq!(check_safety("1 3 6 7 9".to_string()), true);
+
+        // assert_eq!(check_safety("7 6 4 2 1".to_string()), true);
+        // assert_eq!(check_safety("1 2 7 8 9".to_string()), false);
+        // assert_eq!(check_safety("9 7 6 2 1".to_string()), false);
+        // assert_eq!(check_safety("1 3 2 4 5".to_string()), false);
+        // assert_eq!(check_safety("8 6 4 4 1".to_string()), false);
+        // assert_eq!(check_safety("1 3 6 7 9".to_string()), true);
     }
 }
